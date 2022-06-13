f4afa09bd6dea00327890a8080b0bad3b69abdae
Guido <46787485+guidoFortunato@users.noreply.github.com>
2022-06-13 15:11:03 -0300

    comienzo listado de criptomonedas

--- src/components/cryptocurrencies/CryptocurrencyList.jsx
+++ src/components/cryptocurrencies/CryptocurrencyList.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CryptocurrencyList = () => {
+
+  const [coins, setCoins] = useState([]);
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+
+  useEffect(() => {
+    const getData = async()=>{
+      const res = await fetch(url)
+      const data = await res.json()
+      setCoins(data)
+    }
+    getData()
+  }, []);
+  
   return (
     <h2>
       Listado de criptomonedas
+      <ul>
+        {
+          coins.map(item => (
+            <li key={item.symbol}>{item.id}: ${item.current_price}</li>
+          ))
+        }
+      </ul>
     </h2>
   );
 }
